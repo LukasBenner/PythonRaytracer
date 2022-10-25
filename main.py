c763d9ac3608a44b4439381ae389fd15f0a2fc48
NivisPluma <matzeminecraft@gmail.com>
2022-10-25 11:07:55 +0200

    add main funciton

--- main.py
+++ main.py
@@ -11,42 +11,45 @@ from Scene import Scene
 from Texture import *
 from Rect import *
 
-width = 600
-height = 600
-camPosition = np.array([[278], [278], [800]])
-camLookat = np.array([[278], [278], [0]])
+if __name__ == "__main__":
 
-background = np.array([[0.2], [0.2], [0.2]])
+      width = 600
+      height = 600
+      camPosition = np.array([[278], [278], [800]])
+      camLookat = np.array([[278], [278], [0]])
 
-cam = Camera(40.0, camPosition, camLookat, width, height)
+      background = np.array([[0.2], [0.2], [0.2]])
 
-renderer = Renderer(width, height)
+      cam = Camera(40.0, camPosition, camLookat, width, height)
 
-red = Lambertian(.65, .05, .05)
-white = Lambertian(.73, .73, .73)
-green = Lambertian(.12, .45, .15)
-light = DiffuseLight(SolidColor(15,15,15))
+      renderer = Renderer(width, height)
 
-objects = list[Hittable]()
-objects.append(YZRect(0, 555, -555, 0, 0, green)) #left
-objects.append(YZRect(0, 555, -555, 0, 555, red)) #right
-objects.append(XZRect(0, 555, -555, 0, 0, white)) #floor
-objects.append(XZRect(0, 555, -555, 0, 555, white)) #ceiling
-objects.append(XYRect(0, 555, 0, 555, -555, white)) #back
-objects.append(XZRect(213, 343, -332, -227, 553, light)) #light
-box1 = Box(np.array([[130],[0],[-230]]), np.array([[295],[165],[-65]]), red)
-box2 = Box(np.array([[265],[0],[-460]]), np.array([[430],[300],[-295]]), green)
-objects.append(RotateY(box1, 15))
-objects.append(RotateY(box2, -18))
+      red = Lambertian(.65, .05, .05)
+      white = Lambertian(.73, .73, .73)
+      green = Lambertian(.12, .45, .15)
+      light = DiffuseLight(SolidColor(15,15,15))
 
-scene = Scene(objects)
+      objects = list[Hittable]()
+      objects.append(YZRect(0, 555, -555, 0, 0, green)) #left
+      objects.append(YZRect(0, 555, -555, 0, 555, red)) #right
+      objects.append(XZRect(0, 555, -555, 0, 0, white)) #floor
+      objects.append(XZRect(0, 555, -555, 0, 555, white)) #ceiling
+      objects.append(XYRect(0, 555, 0, 555, -555, white)) #back
+      objects.append(XZRect(213, 343, -332, -227, 553, light)) #light
+      box1 = Box(np.array([[130],[0],[-230]]), np.array([[295],[165],[-65]]), red)
+      box2 = Box(np.array([[265],[0],[-460]]), np.array([[430],[300],[-295]]), green)
+      objects.append(RotateY(box1, 15))
+      objects.append(RotateY(box2, -18))
 
-start = time.time()
-renderer.RenderParallel(cam, scene, background)
-end = time.time()
+      scene = Scene(objects)
 
-print("The time of execution of above program is :",
-      (end - start) * 10 ** 3, "ms")
+      start = time.time()
+      # renderer.Render(cam, scene, background)
+      renderer.RenderParallel(cam, scene, background)
+      end = time.time()
 
-image = np.flipud(renderer.image)
-plt.imsave('image.png', image)
+      print("The time of execution of above program is :",
+            (end - start) * 10 ** 3, "ms")
+
+      image = np.flipud(renderer.image)
+      plt.imsave('image.png', image)
